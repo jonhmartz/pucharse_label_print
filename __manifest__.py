755a4136800ad7a938166f02c02e4c2cecaa4208
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-06 13:50:02 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,12 @@
     "version": "1.0",
     "summary": "Impresión de etiquetas 2x1 desde órdenes de compra",
     "description": "Imprime etiquetas desde órdenes de compra.",
-    "category": "Purchases",
+    "category": "Pucharses",
     "author": "TuNombre",
     "depends": ["purchase", "stock"],
     "data": [
-        "views/purchase_label_wizard_view.xml",
-        "views/purchase_order_view.xml"
+        "views/purharse_label_wizard_view.xml",
+        "views/pucharse_order_view.xml"
     ],
     "installable": True,
     "application": False
