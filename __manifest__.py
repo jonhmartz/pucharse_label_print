ef3b67c8856bcb77e208b6105b79a57e96e0921a
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-06 13:47:23 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Etiqueta de Compras 2x1",
+    "version": "1.0",
+    "summary": "Impresión de etiquetas 2x1 desde órdenes de compra",
+    "description": "Imprime etiquetas desde órdenes de compra.",
+    "category": "Purchases",
+    "author": "TuNombre",
+    "depends": ["purchase", "stock"],
+    "data": [
+        "views/purchase_label_wizard_view.xml",
+        "views/purchase_order_view.xml"
+    ],
+    "installable": True,
+    "application": False
+}
