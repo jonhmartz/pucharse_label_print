0485400d7116ce990f73609353261608b9048726
jonhmartz <37033096+jonhmartz@users.noreply.github.com>
2025-04-06 13:51:07 -0600

    Update print_label_wizard.py

--- wizard/print_label_wizard.py
+++ wizard/print_label_wizard.py
@@ -4,7 +4,7 @@ class PrintLabelWizard(models.TransientModel):
     _name = 'print.label.wizard'
     _description = 'Asistente para imprimir etiquetas'
 
-    purchase_id = fields.Many2one('purchase.order', string='Orden de Compra')
+    purchase_id = fields.Many2one('pucharse.order', string='Orden de Compra')
 
     def print_labels(self):
         return self.env.ref('pucharse_label_print.report_purchase_labels').report_action(self.purchase_id)
